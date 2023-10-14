4087ddd6c1d8ce17c657a46d53e57d495c9d6232
shuklakm <82828578+shuklkj@users.noreply.github.com>
2023-10-14 11:01:54 -0500

    add raw dataset

---
+++ eda.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns                       #visualisation
+import matplotlib.pyplot as plt             #visualisation    
+sns.set(color_codes=True)
+
+data_v1 = pd.read_csv("./athletes.csv")
